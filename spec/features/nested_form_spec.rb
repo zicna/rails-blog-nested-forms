f598007c881b3a2bd4d1a48c5961e54921bf82d0
Danny-Sullivan <dsully360@gmail.com>
2015-09-22 15:49:44 -0400

    updated spec

--- spec/features/nested_form_spec.rb
+++ spec/features/nested_form_spec.rb
@@ -13,7 +13,6 @@ describe 'nested form for tag in post', :type => :feature do
 
   it 'can create a post with a new tag' do 
     visit 'posts/new'
-    puts page.body
     within(all('.field').first) do
       fill_in('Name', :with => @post1.name)
       fill_in('Content', :with => @post1.content)
