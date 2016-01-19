d66c73bc80d47d96b102451b8934fce49ad44332
Blake Johnson <blake41@gmail.com>
2016-01-19 16:21:03 -0500

    dont need id permitted in tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => [:id, :name])
+      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => [:name])
     end
 end
