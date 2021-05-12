6fdb65dcfca24d7b4ba13e090d17c696d768341a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-11 19:38:56 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @tag = Tag.new
   end
 
   # GET /posts/1/edit
@@ -68,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [], tags_attributes: [:name])
     end
 end
