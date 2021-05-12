64ef28ebac11ce252255ceb23fd3334dad385d81
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-11 19:55:26 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    # raise params.inspect
     @post = Post.new(post_params)
     respond_to do |format|
       if @post.save
